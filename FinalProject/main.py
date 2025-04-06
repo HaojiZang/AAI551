18ad744408538f4402e019dd26dbb58fe014bcfe
Haoji Zang <89947306+HaojiZang@users.noreply.github.com>
2025-04-06 11:06:45 -0400

    Add files via upload

---
+++ FinalProject/main.py
@@ -0,0 +1,10 @@
+# Author: Haoji Zang, Linpeng Mao, Ruikang Li
+# Date: 04/03/2025
+# Description:
+
+import tkinter as tk
+from auth import login_screen
+
+def main():
+
+    pass
---
+++ FinalProject/nutrition_ui.py
@@ -0,0 +1,7 @@
+# Author: Linpeng Mao
+# Date: 04/03/2025
+# Description:
+
+def nutrition_screen(root, username):
+    # 构建界面：输入框 + 保存按钮
+    pass
