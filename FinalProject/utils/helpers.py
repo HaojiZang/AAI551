e66c90444f7174db8eb33a6bc3ed3f011f430b6a
Jerry03123 <linpeng.mao2022@gmail.com>
2025-04-11 19:07:57 -0400

    Update helpers.py

--- FinalProject/utils/helpers.py
+++ FinalProject/utils/helpers.py
@@ -2,15 +2,16 @@
 # Date: 04/03/2025
 # Description:
 
+import csv
 import os
 from datetime import datetime
 
 def today_str():
-# 返回今天的日期字符串，格式如 "2025-04-03"
-    pass
+    from datetime import datetime
+    return datetime.now().strftime('%Y-%m-%d')
 
 def get_user_data_path(username):
-    return f"data/{username}_data.csv"
+    return os.path.join("data", f"{username}_data.csv")
 
 def get_nutrition_data_path(username):
     return f"data/{username}_nutrition.csv"
@@ -19,8 +20,14 @@ def get_exercise_data_path(username):
     return f"data/{username}_exercise.csv"
 
 def ensure_data_file(username):
-    # 初始化 data 文件夹和 csv
-    pass
+    """Ensure that the user's main data file exists and has a header"""
+    path = get_user_data_path(username)
+    if not os.path.exists("data"):
+        os.makedirs("data")
+    if not os.path.exists(path):
+        with open(path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(["date", "in_cal", "out_cal", "goal"])
 
 def read_csv_as_dict(filename):
     # 读取 csv 返回 dict 列表
